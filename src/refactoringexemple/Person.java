843bace27026b8da19d38f458fd17db07d81ca10
1795904 <1795904@LI-S064-0T10>
2018-05-23 09:48:46 -0400

    Person Refactoring

--- src/refactoringexemple/Person.java
+++ src/refactoringexemple/Person.java
@@ -5,13 +5,73 @@
  */
 package refactoringexemple;
 
+import java.util.Date;
+
 /**
  *
  * @author 1795904
  */
 public class Person {
-    int age;
-    public void study(){
-        System.out.println("I study.");
+    private int age;
+    private Date dateBirth;
+    private String firstName;
+    private String lastName;
+    
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
     }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the dateBirth
+     */
+    public Date getDateBirth() {
+        return dateBirth;
+    }
+
+    /**
+     * @param dateBirth the dateBirth to set
+     */
+    public void setDateBirth(Date dateBirth) {
+        this.dateBirth = dateBirth;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    
 }
