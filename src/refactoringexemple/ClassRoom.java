6e80e754d8848c604737183f25e266675fd3ac4c
1795904 <1795904@LI-S064-0T10>
2018-05-23 09:36:26 -0400

    ClassRoom Superclass of Lab created

---
+++ src/refactoringexemple/ClassRoom.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package refactoringexemple;
+
+/**
+ *
+ * @author 1795904
+ */
+public class ClassRoom {
+    
+    String idCalss;
+    Integer capacity;
+
+    public String getIdCalss() {
+        return idCalss;
+    }
+
+    public void setIdCalss(String idCalss) {
+        this.idCalss = idCalss;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+    
+}
