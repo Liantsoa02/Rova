6278d1e27db7a74ee3d5f7ed7c54562cf5e90a17
1795904 <1795904@LI-S064-0T10>
2018-05-23 08:57:49 -0400

    Lab creation

--- src/refactoringexemple/Lab.java
+++ src/refactoringexemple/Lab.java
@@ -10,5 +10,33 @@ package refactoringexemple;
  * @author 1795904
  */
 public class Lab {
+    String idCalss;
+    Integer capacity;
+    Integer nbComputers;
+
+    public String getIdCalss() {
+        return idCalss;
+    }
+
+    public void setIdCalss(String idCalss) {
+        this.idCalss = idCalss;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public Integer getNbComputers() {
+        return nbComputers;
+    }
+
+    public void setNbComputers(Integer nbComputers) {
+        this.nbComputers = nbComputers;
+    }
+    
     
 }
